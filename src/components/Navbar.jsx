bdc405de308afd93a4d05b2df6a92dbaa917f64e
Aldi <aldivauzatamma@gmail.com>
2026-05-14 14:12:27 +0700

    perbesar lagi logo navbar (36->40px)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const Logo = () => (
-  <img src="/logo-color.png" alt="Edupongo" style={{ height: 36, width: 'auto', flexShrink: 0 }} />
+  <img src="/logo-color.png" alt="Edupongo" style={{ height: 40, width: 'auto', flexShrink: 0 }} />
 );
 
 export default function Navbar({ page, onNavigate }) {
